08b29c58ea42b55d99c29b697e53a03687dbcbd5
Babatunde Fortune-Itiola <itiola579@gmail.com>
2024-05-14 07:08:59 +0100

    hi

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
-st.title("I am here")
-st.header("I am here")
-st.subheader("I am here")
-st.text("I am here")
+st.title("Smart Credit")
+#st.header("I am here")
+st.subheader("This Application predicts the credit worthiness of a customer")
+#st.text("I am here")
+
+st.selectbox("Gender",["Male","Female"])
+st.selectbox("Married",["Yes","No"])
+st.number_input("No of Dependent", 0, 10)
+st.selectbox("Education (Up to Secondary Level)",["Yes","No"])
+st.selectbox("Self Employed",["Yes","No"])
+st.number_input("Enter Loan Amount", 0, 1000000000)
+st.number_input("Enter Loan Amount Term", 0, 1000)
+st.selectbox("Bad Credit History",["Yes","No"])
+st.button("Predict Credit Worthiness", type="primary")
